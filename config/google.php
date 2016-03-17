f26ada0f35014ab021a98f7bbfa4dcabd6f1a4cb
Harsha <harsha.reaper@gmail.com>
2016-03-17 16:00:18 +0530

    Adding Files

---
+++ config/google.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['clientId'] = 'XXXX'; //add your client id
+$config['clientSecret'] = 'XXXX'; //add your client secret
+$config['redirectUri'] = 'XXXX'; //add your redirect uri
+
+?>
