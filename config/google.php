1b8b99f67e28a0de268dc5618a1c2f7bad9426d1
Nicholas Humphries <Humni@users.noreply.github.com>
2016-12-17 11:28:16 +1300

    Update google.php
    Added the application name, should be set in config file

--- config/google.php
+++ config/google.php
@@ -4,5 +4,6 @@ $config['clientId'] = 'XXXX'; //add your client id
 $config['clientSecret'] = 'XXXX'; //add your client secret
 $config['redirectUri'] = 'XXXX'; //add your redirect uri
 $config['apiKey'] = ''; //add your api key here
+$config['applicationName'] ='Application Name'; //application name for the api
 
 ?>
